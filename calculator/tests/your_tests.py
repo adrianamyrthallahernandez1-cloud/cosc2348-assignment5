abea90865151b3bb772a86e0d197efd706ac4f3b
Adriana Hernandez <ahernandez242@islander.tamucc.edu>
2026-04-24 04:40:08 +0000

    Complete Assignment 6

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -4,7 +4,21 @@ from calculator_adapter import run
 
 ### ADD AT LEAST TWO TESTS HERE!
 
+def test_addition():
+    assert run(["2", "+", "3"]) == "The result of your operation is 5."
+
+def test_subtraction():
+    assert run(["10", "-", "4"]) == "The result of your operation is 6."
+
+
+print("All tests passed!")#!/usr/bin/env python3
+from calculator_adapter import run
+
+
+### ADD AT LEAST TWO TESTS HERE!
+
 
 ###
 
 print("All tests passed!")
+
